ec44c5507935e77c6a82249d748ce924128f4f33
leonardocapozzi <leonardo.capozzi.1995@gmail.com>
2021-10-22 23:54:25 +0200

    chore: defined calendar filter

--- js/dataset/dataset-filter.js
+++ js/dataset/dataset-filter.js
@@ -5,11 +5,53 @@ var dataSetFilter = (function() {
     var data = dataSetFactory.getInstance().data;    
 
     function filterByPropertyEqualTo(propertyName, propertyValue) {
+
+        if(propertyName == "Start_Time") {
+            return data.filter(item => {
+                var date = new Date(item[propertyName]);
+                var year = date.getFullYear();
+
+                return propertyValue == year ? true : false;
+            });
+        }
+
+        console.log("PropertyName is different to Start_Time");
+
         return data.filter(item => item[propertyName] == propertyValue);
     }
 
+    function filterByPropertiesAtMostOneEqualTo(propertyName, values) {
+
+        if(propertyName == "Start_Time") {
+            return data.filter(item => {
+                var date = new Date(item[propertyName]);
+                var year = date.getFullYear();
+
+                for(var i = 0; i < values.length; i ++) {
+                    if(values[i] == year) {
+                        return true;
+                    }
+                }
+    
+                return false;
+            });
+        }
+
+        return data.filter(item => {
+
+            for(var i = 0; i < values.length; i ++) {
+                if(values[i] == item[propertyName]) {
+                    return true;
+                }
+            }
+
+            return false;
+        });
+    }
+
     return {
-        filterByPropertyEqualTo: filterByPropertyEqualTo
+        filterByPropertyEqualTo: filterByPropertyEqualTo,
+        filterByPropertiesAtMostOneEqualTo: filterByPropertiesAtMostOneEqualTo
     };
 
 })();
--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ import { dataSetFactory } from "./dataset/dataset.js";
 import { PCAScatterPlotBuilder } from "./pca/pca-script.js";
 import { BubbleMapBuilder } from "./map.js";
 
-document.getElementById("time-wrapper").addEventListener('change', function(event) {
+document.getElementById("time-filter-container").addEventListener('change', function(event) {
     
     //Get checkboxs
     var dayCheck = document.getElementById("day-checkbox");
@@ -24,6 +24,32 @@ document.getElementById("time-wrapper").addEventListener('change', function(even
     else {
         data = dataSetFactory.getInstance().data;
     }
+
+    PCAScatterPlotBuilder.redraw(data);
+    BubbleMapBuilder.redraw(data);
+});
+
+document.getElementById("calendar-filter-container").addEventListener('change', function(event) {
+    
+    //Get checkboxs
+    var checkboxs = document.getElementsByName("year-checkbox");
+
+    var values = new Array();
+
+    var k = 0;
+
+    for(var i = 0; i < checkboxs.length; i ++) {
+        if(checkboxs[i].checked) {
+            values[k] = checkboxs[i].value;
+            k ++;
+        }
+    }
+
+    console.log("Values to filter by year(s) => ", values);
+
+    var data = dataSetFilter.filterByPropertiesAtMostOneEqualTo("Start_Time", values);
+
+    console.log("Data post calendar filter: ", data);
 
     PCAScatterPlotBuilder.redraw(data);
     BubbleMapBuilder.redraw(data);
